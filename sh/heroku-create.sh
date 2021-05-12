4f2e2d02afbc1ab6160ed2ee0ac2015fcf414823
Felippe Regazio <felippe.moraes@zoho.com>
2021-05-12 00:14:19 -0300

    heroku-create script done

--- sh/heroku-create.sh
+++ sh/heroku-create.sh
@@ -2,19 +2,12 @@
 
 heroku create cvkeep-frontend
 
-heroku config:set VUE_APP_BRAND_NAME="CV Keep"
-heroku config:set VUE_APP_API_URL="http://localhost:5000"
-heroku config:set VUE_APP_AUTH_AVAILABLE="email facebook github likedin"
-heroku config:set VUE_APP_FACEBOOK_API_VERSION="v7.0"
-heroku config:set VUE_APP_FACEBOOK_API_ID=""
-heroku config:set VUE_APP_LINKEDIN_API_KEY=""
-heroku config:set VUE_APP_CONTACT_EMAIL="contact@email.com"
-heroku config:set VUE_APP_DONATION_PAYPAL_URL=""
-heroku config:set VUE_APP_DONATION_PIX_KEY=""
-heroku config:set VUE_APP_DONATION_BITCOIN=""
-heroku config:set FRONT_SERVER_APP_URL="https://cvkeep-frontend.herokuapp.com/"
-heroku config:set SEO_CARD_IMAGE_URL="https://cvkeep-frontend.herokuapp.com/seo/card.png"
-heroku config:set SEO_TWITTER_USER="username"
+for ITEM in "VUE_APP_BRAND_NAME" "VUE_APP_API_URL" "VUE_APP_AUTH_AVAILABLE" "VUE_APP_FACEBOOK_API_VERSION" "VUE_APP_FACEBOOK_API_ID" "VUE_APP_LINKEDIN_API_KEY" "VUE_APP_CONTACT_EMAIL" "VUE_APP_DONATION_PAYPAL_URL" "VUE_APP_DONATION_PIX_KEY" "VUE_APP_DONATION_BITCOIN" "FRONT_SERVER_APP_URL" "SEO_CARD_IMAGE_URL" "SEO_TWITTER_USER";
+do
+  echo "Enter value for env key $ITEM:"
+  read VALUE
+  heroku config:set $ITEM="$VALUE"
+done
 
 git push heroku main
 heroku ps:scale web=1
